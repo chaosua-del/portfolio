5e36fd68655d546d052d7cc3c04692832f5676c7
chaosua-del <d.yuhymenko@gmail.com>
2024-05-11 20:28:28 +0200

    Remove modules from index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,5 @@
-import { Box } from "@chakra-ui/react";
-
-import { DecoratedLayout } from "@/layouts";
-import {
-  Blog,
-  Feedback,
-  Intro,
-  Pricing,
-  Questions,
-  Solutions
-} from "@/modules/Home/widgets";
-
 const page = () => {
-  return (
-    <DecoratedLayout>
-      <Box>
-        <Intro />
-        <Solutions />
-        <Feedback />
-        <Pricing />
-        <Questions />
-        <Blog />
-      </Box>
-    </DecoratedLayout>
-  );
+  return <>Hello world</>;
 };
 
 export default page;
