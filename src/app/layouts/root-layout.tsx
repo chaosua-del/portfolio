7976edb72c17eef6694c887faef0fb57f9ed9b67
chaosua-del <d.yuhymenko@gmail.com>
2024-11-20 14:32:06 +0100

    remove i18n

--- src/app/layouts/root-layout.tsx
+++ src/app/layouts/root-layout.tsx
@@ -1,36 +1,11 @@
-import { useEffect, useRef } from 'react';
-import { useTranslation } from 'react-i18next';
-import { Outlet, useSearchParams } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
-import type { Language } from '@/lib/i18n/i18n';
-import { supportedLngs } from '@/lib/i18n/i18n';
 import { Navbar } from '@/components/navbar';
 import { ContactMe } from '@/features/home/contact-me';
 
 export default function RootLayout() {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const { i18n } = useTranslation();
-
-  const redirectLang = searchParams.get('redirectLang');
-
-  const pdfRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (
-      redirectLang &&
-      supportedLngs.includes(redirectLang.toLowerCase() as Language)
-    ) {
-      i18n.changeLanguage(redirectLang);
-
-      const updatedSearchParams = new URLSearchParams(searchParams);
-      updatedSearchParams.delete('redirectLang');
-      setSearchParams(updatedSearchParams); // Set the updated instance
-    }
-  }, [i18n, redirectLang, searchParams, setSearchParams]);
-
   return (
-    <div ref={pdfRef} className="max-w-[1100px] mx-auto">
+    <div className="max-w-[1100px] mx-auto">
       <Navbar />
       <div className="w-full px-8">
         <Outlet />
