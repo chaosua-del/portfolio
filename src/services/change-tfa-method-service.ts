7288ace7f9cf9018d3b57295ca6173b55dff134a
chaosua-del <d.yuhymenko@gmail.com>
2024-11-29 15:21:27 +0100

    fix: remove unsused func

--- src/services/change-tfa-method-service.ts
+++
@@ -1,8 +0,0 @@
-import { apiClient } from '@/lib/api-client';
-
-export async function changeTfaMethodService(method: string | null) {
-  const res = await apiClient.patch<{ tfaLink: string }>('/auth/2fa/change', {
-    method,
-  });
-  return res.data;
-}
--- src/services/get-exchange-rates-service.ts
+++
@@ -1,8 +0,0 @@
-import { apiClient } from '@/lib/api-client';
-
-export async function getExchangeRatesService() {
-  const res = await apiClient.get<Record<string, number>>(
-    '/assets/exchange-rates',
-  );
-  return res.data;
-}
